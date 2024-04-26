068db97c85ca8aec8bd58efed1be85805f8af91b
noor-rutjes <n.rutjes@outlook.com>
2024-04-26 12:23:39 +0200

    added register and login function, styling needs to be done

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext.jsx';
+import axios from 'axios';
+
+function Profile() {
+  const endpoint = "https://frontend-educational-backend.herokuapp.com/";
+  const [profileData, setProfileData] = useState({});
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    // we halen de pagina-content op in de mounting-cycle
+    async function fetchProfileData() {
+      // haal de token uit de Local Storage om in het GET-request te bewijzen dat we geauthoriseerd zijn
+      const token = localStorage.getItem('token');
+
+      try {
+        const result = await axios.get(`${endpoint}api/user`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setProfileData(result.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    void fetchProfileData();
+  }, [])
+
+  return (
+      <>
+        <h1>Profielpagina</h1>
+        <section>
+          <h2>Gegevens</h2>
+          <p><strong>Gebruikersnaam:</strong> {user.username}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+        </section>
+
+        {/*Als er keys in ons object zitten hebben we data, en dan renderen we de content*/}
+        {/*{Object.keys(profileData).length > 0 &&*/}
+        {/*    <section>*/}
+        {/*      <h2>Strikt geheime profiel-content</h2>*/}
+        {/*      <h3>{profileData.title}</h3>*/}
+        {/*      <p>{profileData.content}</p>*/}
+        {/*    </section>*/}
+        {/*}*/}
+        <p>Terug naar de <Link to="/">Homepagina</Link></p>
+      </>
+  );
+}
+
+export default Profile;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,74 @@
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
+
+function SignIn() {
+  const endpoint = "https://frontend-educational-backend.herokuapp.com/";
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, toggleError] = useState(false);
+  const { login } = useContext(AuthContext);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    toggleError(false);
+
+    try {
+      const result = await axios.post(`${endpoint}api/auth/signin`, {
+        "username": username,
+        "password": password,
+      });
+      // log het resultaat in de console
+      console.log(result.data);
+
+      // geef de JWT token aan de login-functie van de context mee
+      login(result.data.accessToken);
+
+    } catch(e) {
+      console.error(e);
+      toggleError(true);
+    }
+  }
+
+  return (
+      <>
+        <h1>Inloggen</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username-field">
+            Emailadres:
+            <input
+                type="username"
+                id="username-field"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+
+          <label htmlFor="password-field">
+            Wachtwoord:
+            <input
+                type="password"
+                id="password-field"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          {error && <p className="error">Combinatie van gebruikersnaam en wachtwoord is onjuist</p>}
+
+          <button
+              type="submit"
+              className="form-button"
+          >
+            Inloggen
+          </button>
+        </form>
+
+        <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
+      </>
+  );
+}
+
+export default SignIn;
